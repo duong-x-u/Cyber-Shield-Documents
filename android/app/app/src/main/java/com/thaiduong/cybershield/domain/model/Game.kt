b6317b32c6b8286cfc66ecbb8b895376c9a7b22e
pham-thai-duong-2010-vietnam <quoctrieu581@gmail.com>
2025-09-10 17:36:12 +0700

    chore: reset repo empty

--- android/app/app/src/main/java/com/thaiduong/cybershield/domain/model/Game.kt
+++
@@ -1,7 +0,0 @@
-package com.thaiduong.cybershield.domain.model
-
-data class Game(
-    val packageName: String,
-    val displayName: String,
-    val isHeavyGame: Boolean = false
-)
--- android/app/app/src/main/java/com/thaiduong/cybershield/domain/repository/GameRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.thaiduong.cybershield.domain.repository
-
-import com.thaiduong.cybershield.domain.model.Game
-
-interface GameRepository {
-    fun getHeavyGames(): Set<Game>
-    fun isHeavyGame(packageName: String): Boolean
-}
